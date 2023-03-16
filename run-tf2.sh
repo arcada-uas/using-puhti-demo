834880c5b0a1f2395c50cf6d4d3fdb48031e0b1f
Shcherbakov <parland@arcada.fi>
2023-03-16 13:54:41 +0200

    Update run-tf2.sh

--- run-tf2.sh
+++ run-tf2.sh
@@ -6,13 +6,13 @@
 #SBATCH --gres=gpu:v100:1
 #SBATCH --time=0:15:00
 #SBATCH --mem=64G
-#SBATCH --account=project_2005604
+#SBATCH --account=project_2000859
 
 module purge
 module load tensorflow/2.0.0
 module list
 
-export PROJ="2005604"
+export PROJ="2000859"
 export DATADIR=$HOME/puhti-work/scratch_$PROJ
 
 set -xv
